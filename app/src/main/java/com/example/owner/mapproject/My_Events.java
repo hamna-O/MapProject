ce76e209bcf3aca494d5b43d712c2c93f2c30a38
hamna-O <hamna810@gmail.com>
2017-04-11 12:04:19 +0530

    my events

--- app/src/main/java/com/example/owner/mapproject/My_Events.java
+++ app/src/main/java/com/example/owner/mapproject/My_Events.java
@@ -14,7 +14,7 @@ import android.widget.ListView;
 
 public class My_Events extends Fragment{
     View myView;
-  //  ListView event_list;
+    ListView event_list;
     /*@Override
   public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
@@ -23,8 +23,8 @@ public class My_Events extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView=inflater.inflate(R.layout.my_events,container,false);
+        event_list= (ListView) myView.findViewById(R.id.listview_my_events);
         return myView;
-    //   event_list= (ListView) myView.findViewById(R.id.listview_my_events);
     }
 
 }
