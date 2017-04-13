230ef5cec3339db1f1d33aae6b51376edf29d796
hamna-O <hamna810@gmail.com>
2017-04-13 15:05:20 +0530

    create_event

--- app/src/main/java/com/example/owner/mapproject/Create_Event.java
+++ app/src/main/java/com/example/owner/mapproject/Create_Event.java
@@ -6,6 +6,13 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by Khadeeja on 02-03-2017.
@@ -13,11 +20,41 @@ import android.view.ViewGroup;
 
 public class Create_Event extends Fragment{
     View myView;
+    Button b1;
+    EditText title, venue, date, time, des;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView=inflater.inflate(R.layout.create_event,container,false);
+        b1= (Button) myView.findViewById(R.id.button);
+        title= (EditText) myView.findViewById(R.id.e_title);
+        date= (EditText) myView.findViewById(R.id.e_date);
+        time= (EditText) myView.findViewById(R.id.e_time);
+        venue= (EditText) myView.findViewById(R.id.e_venue);
+        des= (EditText) myView.findViewById(R.id.e_des);
+      //  cat= (EditText) myView.findViewById(R.id.e);
+     /*   FirebaseDatabase database =FirebaseDatabase.getInstance();
+        DatabaseReference ref =database.getReference("message");
+        ref.setValue("data");*/
+
+       b1.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+             //  Toast.makeText(getContext(),"hhgbs",Toast.LENGTH_LONG).show();
+             //  Firebase Myref =new Firebase("https://mymapsapp-152515.firebaseio.com/");
+               FirebaseDatabase database =FirebaseDatabase.getInstance();
+               DatabaseReference ref =database.getReference("message");
+               ref.setValue("data");
+           }
+       });
         return myView;
     }
 }
