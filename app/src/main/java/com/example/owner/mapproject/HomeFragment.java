6f513c2a581ff8a8318baaee672415b5603da1c5
Shabeeb <mhdshb007@live.com>
2017-04-13 12:48:03 +0530

    Marker added

--- app/src/main/java/com/example/owner/mapproject/HomeFragment.java
+++ app/src/main/java/com/example/owner/mapproject/HomeFragment.java
@@ -1,34 +1,62 @@
 package com.example.owner.mapproject;
 
-import android.app.Fragment;
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class HomeFragment extends FragmentActivity implements OnMapReadyCallback {
 
-public class HomeFragment extends Fragment {
-    View myView;
     private GoogleMap mMap;
     private Marker m;
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        myView=inflater.inflate(R.layout.fragment_home,container,false);
+//    MapView mapView;
 
-/*
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager(). fin
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-*/
-
-
-        return myView;
     }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(11.255802, 75.6707252);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney").draggable(true));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(10,10))
+                .title("Hello world"));
+
+
+
+
+    }
+
 }
---
+++ app/src/main/java/com/example/owner/mapproject/MapsHomeFragment.java
@@ -0,0 +1,49 @@
+package com.example.owner.mapproject;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsHomeFragment extends Fragment implements OnMapReadyCallback {
+
+
+    private  GoogleMap mMap;
+    private Marker m;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_maps_home, container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MapFragment fragment = (MapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        LatLng kottakkal = new LatLng(11.0001,75.9988);
+        mMap.addMarker(new MarkerOptions().position(kottakkal).title("Marker in Kottttakkal").draggable(true));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(kottakkal));
+
+    }
+}
