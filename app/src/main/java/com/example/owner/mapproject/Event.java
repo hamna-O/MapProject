c2f5bf1d3edb6e15e5009a8228194fdbe9901023
hamna-O <hamna810@gmail.com>
2017-04-15 12:06:54 +0530

    waste

---
+++ app/src/main/java/com/example/owner/mapproject/Event.java
@@ -0,0 +1,17 @@
+package com.example.owner.mapproject;
+
+/**
+ * Created by owner on 4/15/2017.
+ */
+
+public class Event {
+    String Title;
+    String Des;
+    public Event(){}
+    public Event(String title, String des){
+        this.Title=title;
+        this.Des=des;
+
+    }
+
+}
--- app/src/test/java/Event.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by owner on 4/14/2017.
- */
-
-public class Event {
-
-}
