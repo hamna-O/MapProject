fb774197ef39eb06234f77a2b6f5872c31d0c41e
hamna-O <hamna810@gmail.com>
2017-04-15 13:24:50 +0530

    database work

---
+++ app/src/main/java/com/example/owner/mapproject/Models/User.java
@@ -0,0 +1,12 @@
+package com.example.owner.mapproject.Models;
+
+/**
+ * Created by owner on 4/15/2017.
+ */
+
+public class User {
+    public String _id;
+    public  String email;
+    public  String name;
+
+}
